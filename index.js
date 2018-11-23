76bacd818ac27a7070463d84967d2f0c3d0d20d9
Ally Netzel <netzelally@gmail.com>
2018-11-23 23:13:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function printBadges(names){
+  for (let i = 0; i < names.length; i++){
+    console.log(`Welcome ${names[i]}! You are employee #${i+1}.` )
+  }
+  return names;
+}
+
+function tailsNeverFails(){
+  return Math.random() >= 0.5;
+}
+let countdown = 10;
+while(tailsNeverFails()){
+  console.log('You got 2 tails in a row.')
+  
+}
