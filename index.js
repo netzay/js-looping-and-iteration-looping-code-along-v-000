72ac81c7e5783c0fa51fb22e33d0e30e899f699e
Ally Netzel <netzelally@gmail.com>
2018-11-23 23:13:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,11 +6,10 @@ function printBadges(names){
   return names;
 }
 
-function tailsNeverFails(){
-  return Math.random() >= 0.5;
-}
-let countdown = 10;
-while(tailsNeverFails()){
-  console.log('You got 2 tails in a row.')
-  
+function tailsNeverFails() {
+  let tails = 0;
+  while (Math.random() >= 0.5) {
+    tails++;
+  }
+  return `You got ${tails} tails in a row!`;
 }
